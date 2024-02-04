80cff12684f17f710b1848e20c2be2ebcebbf15b
Lui Habl <txaicalvoso@gmail.com>
2024-02-04 09:39:01 -0300

    Add test

--- test/test.c
+++ test/test.c
@@ -38,6 +38,39 @@ void test_rect_pack_one_rect_per_page_paging() {
     }
 }
 
+void test_rect_pack_fill() {
+    rect_r rects[100];
+
+    for(int i = 0; i < sizeof(rects)/sizeof(rect_r); i++) {
+        rects[i].id = i;
+        rects[i].w = 1;
+        rects[i].h = 1;
+    }
+
+    TEST_ASSERT_TRUE(rect_pack(10, 10, false, rects, sizeof(rects)/sizeof(rect_r)));
+    
+    for(int i = 0; i < sizeof(rects)/sizeof(rect_r); i++) {
+        TEST_ASSERT_TRUE(rects[i].info.packed);
+        TEST_ASSERT_EQUAL(rects[i].info.page, 0);
+    }
+}
+
+void test_rect_pack_no_paging() {
+    rect_r rects[] = {
+        {.id = 0, .w = 6, .h = 7},
+        {.id = 1, .w = 8, .h = 7},
+        {.id = 2, .w = 8, .h = 9},
+        {.id = 3, .w = 10, .h = 9},
+    };
+
+    TEST_ASSERT_FALSE(rect_pack(10, 10, false, rects, sizeof(rects)/sizeof(rect_r)));
+
+    TEST_ASSERT_TRUE(rects[0].info.packed);
+    TEST_ASSERT_FALSE(rects[1].info.packed);
+    TEST_ASSERT_FALSE(rects[2].info.packed);
+    TEST_ASSERT_FALSE(rects[3].info.packed);
+}
+
 void test_rect_pack_fail_paging() {
 
     rect_r rects[2];
@@ -62,6 +95,7 @@ int main() {
     RUN_TEST(test_rect_pack_uniform_paging);
     RUN_TEST(test_rect_pack_fail_paging);
     RUN_TEST(test_rect_pack_one_rect_per_page_paging);
+    RUN_TEST(test_rect_pack_fill);
 
     return UNITY_END();
 }
